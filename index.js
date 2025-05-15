9a1733c751ebe7ec9ce8771cd8f28440ffe46b64
Leancba <leandro.milia96@gmail.com>
2025-05-15 17:05:12 -0300

    last update

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { PaperProvider } from 'react-native-paper';
-import { lightTheme } from './theme.config';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 
 const Main = () => {
 	return (
-		<PaperProvider theme={lightTheme}>
+		<PaperProvider>
 			<Provider store={store}>
 				<App />
 			</Provider >
